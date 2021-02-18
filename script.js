cccd0b0f1d01be95c85ea4fdddad05431e95c6c5
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-18 18:05:39 +0000

    spaces

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.getElementById('click').addEventListener('click', randomNum)
 
 let clicker = 0
 
-function randomNum() {
+function randomNum () {
   clicker = document.getElementById('table').value
   clicker = parseInt(clicker)
 
   if (clicker > 0) {
     document.getElementById('name').innerHTML = 'Positive'
-   } else {
+  } else {
     document.getElementById('name').innerHTML = 'Negative'
   }
 }
